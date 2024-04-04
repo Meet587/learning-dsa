3af4e62255d45c36b11fc48f6744e92df6012bf6
Meet Rakholiya <meet@investifyd.com>
2024-04-04 10:34:34 +0530

    array append and inser functions

--- array_adt/01_array_adt.cpp
+++ array_adt/01_array_adt.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 struct Array
 {
-    int *A;
-    // int A[20];
+    // int *A;
+    int A[20];
     int size;
     int length;
 };
@@ -18,30 +18,53 @@ void Display(struct Array arr)
     printf("\nElements are\n");
     for (i = 0; i < arr.length; i++)
     {
-        printf("%d", arr.A[i]);
+        printf("%d \n", arr.A[i]);
+    }
+}
+
+void Append(struct Array *arr, int x)
+{
+    if (arr->length < arr->size)
+    {
+        arr->A[arr->length++] = x;
+    }
+}
+
+void Insert(struct Array *arr, int index, int x)
+{
+    if (index >= 0 && arr->size >= index)
+    {
+        for (int i = arr->length; i > index; i--)
+        {
+            arr->A[i] = arr->A[i - 1];
+        }
+        arr->A[index] = x;
+        arr->length++;
     }
 }
 
 int main()
 {
-    struct Array arr;
-    int n, i;
+    struct Array arr = {{2, 3, 4, 5, 6}, 10, 5};
+    // int n, i;
 
-    printf("enter size on an array");
-    scanf("%d", &arr.size); 
-    arr.A = (int *)malloc(arr.size * sizeof(int));
-    arr.length = 0;
+    // printf("enter size on an array");
+    // scanf("%d", &arr.size);
+    // arr.A = (int *)malloc(arr.size * sizeof(int));
+    // arr.length = 0;
 
-    printf("Enter number of elements.\n");
-    scanf("%d", &n);
+    // printf("Enter number of elements.\n");
+    // scanf("%d", &n);
 
-    printf("Enter all elements\n");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr.A[i]);
-    }
-    arr.length = n;
+    // printf("Enter all elements\n");
+    // for (i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &arr.A[i]);
+    // }
+    // arr.length = n;
 
+    Insert(&arr, 2, 10);
+    // Append(&arr, 9);
     Display(arr);
 
     return 0;
